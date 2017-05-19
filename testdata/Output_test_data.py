afafa068ce5b5ca50ca1d2ec7daab3d52ffca736
Teus Hagen <teus@theunis.org>
2017-05-19 12:32:46 +0200

    initialize

---
+++ testdata/Output_test_data.py
@@ -0,0 +1,25 @@
+ident = {
+    'geolocation': '51.420635,6.1356117,22.9',
+    'fields': ['time', 'pm_25', 'pm_10', 'dtemp', 'drh', 'temp', 'rh', 'hpa'],
+    'extern_ip': ['83.161.151.250'],
+    'label': 'alphaTest',
+    'project': 'BdP',
+    'version': '0.2.28',
+    'units': ['s', 'pcs/qf', 'pcs/qf', 'C', '%', 'C', '%', 'hPa'],
+    'intern_ip': ['192.168.178.49', '2001:980:ac6a:1:83c2:7b8d:90b7:8750', '2001:980:ac6a:1:17bf:6b65:17d2:dd7a'],
+    'serial': 'test_sense',
+    'types': ['Dylos DC1100', 'DHT22', 'BME280']
+    }
+data = [
+    {'drh': 29.3, 'pm_25': 318.0, 'temp': 28.75,
+    'time': 1494777772, 'hpa': 712.0, 'dtemp': 27.8,
+    'rh': 25.0, 'pm_10': 62.0},
+    {'drh': 29.5, 'pm_25': 318.0, 'temp': 27,
+    'time': 1494778772, 'hpa': 714.0, 'dtemp': 27.8,
+    'geolocation': '51.433,6.22,13',
+    'rh': 26.0, 'pm_10': 63.0},
+    {'drh': 29.6, 'pm_25': 318.0, 'temp': 27.5,
+    'null': None,
+    'time': 1494779772, 'hpa': 715.0, 'dtemp': 27.8,
+    'rh': 28.0, 'pm_10': 64.0},
+]
