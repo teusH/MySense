f8af51fbb84fd5c626526b120215c5f7a89ebee0
Teus Hagen <teus@theunis.org>
2017-06-18 15:59:54 +0200

    corrected typo

--- MyDYLOS.py
+++ MyDYLOS.py
@@ -18,7 +18,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-# $Id: MyDYLOS.py,v 2.19 2017/06/06 14:15:01 teus Exp teus $
+# $Id: MyDYLOS.py,v 2.20 2017/06/10 15:26:29 teus Exp teus $
 
 # TO DO: open_serial function may be needed by other modules as well?
 #       add version number, firmware number
@@ -34,7 +34,7 @@
     MET/ONE BAM1020 = Dylos + 5.98 (rel.hum*corr see Dexel University report)
 """
 modulename='$RCSfile: MyDYLOS.py,v $'[10:-4]
-__version__ = "0." + "$Revision: 2.19 $"[11:-2]
+__version__ = "0." + "$Revision: 2.20 $"[11:-2]
 
 # configurable options
 __options__ = [
@@ -192,7 +192,8 @@ def registrate():
     if not Conf['input']: return False
     if Conf['fd'] != None: return True
     Conf['input'] = False
-    if (Conf['type'] == None) or (Conf['type'][0:8].lower() != 'dylos dc'):
+    if (Conf['type'] == None) or (Conf['type'][0:5].lower() != 'dylos'):
+        MyLogger.log(modulename,'ERROR','Incorrect Dylos type: %s' % Conf['type'])
         return False
     if not open_serial():
         return False
