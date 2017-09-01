a9f473c288ec30ebdffdc1ab62731371cbe837ea
Teus Hagen <teus@theunis.org>
2017-09-01 21:13:47 +0200

    improved check on DHT read errors

--- MyDHT.py
+++ MyDHT.py
@@ -18,7 +18,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-# $Id: MyDHT.py,v 2.28 2017/08/24 13:01:27 teus Exp teus $
+# $Id: MyDHT.py,v 2.29 2017/09/01 19:10:48 teus Exp teus $
 
 # TO DO: make a threat to read every period some values
 # DHT import module can delay some seconds
@@ -28,7 +28,7 @@
     Relies on Conf setting by main program
 """
 modulename='$RCSfile: MyDHT.py,v $'[10:-4]
-__version__ = "0." + "$Revision: 2.28 $"[11:-2]
+__version__ = "0." + "$Revision: 2.29 $"[11:-2]
 __license__ = 'GPLV4'
 
 try:
@@ -109,7 +109,7 @@ def Add(conf):
             MyLogger.log(modulename,'DEBUG',"Rel.Humidity: %5.2f %% not calibrated" % humidity)
     else:
         MyLogger.log(modulename,'DEBUG',"Rel.Humidity: None")
-    if (temp == 0.0) and (humidity == 0.0): return rec
+    if (temp <= 0.0) and (humidity <= 0.0): return rec
     if ('raw' in conf.keys()) and (conf['raw'] != None):
         conf['raw'].publish(
             tag='%s' % conf['type'].lower(),
