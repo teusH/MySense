593edf1411bcccb871cafef3b54cc9be7f0b2894
Teus Hagen <teus@theunis.org>
2018-03-28 17:35:40 +0200

    deleted

--- PyCom/LoPy/firmware/LoRaConfig.py
+++
@@ -1,6 +0,0 @@
-# Replace with your own OTAA keys,
-# obtainable through the "files" widget in Managed IoT Cloud.
-# TTN register, app id Yours123456, dev id YourDevice123456789
-dev_eui = "XXXXXXXXXXXXXXXX"
-app_eui = "YYYYYYYYYYYYYYYY"
-app_key = "ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ"
