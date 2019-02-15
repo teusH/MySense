445a2522b1b4188726df8559e5d6ac74094b2df9
Teus Hagen <teus@theunis.org>
2019-02-15 17:52:44 +0100

    easier example for auto config

--- PyCom/Config.py
+++ PyCom/Config.py
@@ -18,7 +18,7 @@ W_SSID = 'MySense-PyCom'
 W_PASS = 'acacadabra'
 
 # define 0 if not used (GPS may overwrite this)
-thisGPS = [0.0,0.0,0.0] # (LAT,LON,ALT)
+# thisGPS = [0.0,0.0,0.0] # (LAT,LON,ALT)
 
 # uncomment if calibration is known
 # # date, reference to ?
@@ -36,13 +36,14 @@ thisGPS = [0.0,0.0,0.0] # (LAT,LON,ALT)
 # auto detect I2C address if module is wired/connected
 # Meteo: BME280, BME680, SHT31
 # meteo module is auto detected
-useMeteo = 'I2C'# I2C bus, None: disabled
-# only needed for BME680, if not defined recalculate takes 5 minutes
-M_gBase = 430940.4 # BME680 gas base line (dflt None)
+# useMeteo = 'I2C'# I2C bus, None: disabled
+# if BME680 is used:
+# M_gBase = 430940.4 # BME680 gas base line (dflt None or missing: recalculate)
 
 # use oled display None: disabled
-useSSD = 'I2C'
-#useSSD = 'SPI'
+#useSSD = 'I2C' # deprecated
+#useSSD = 'SPI' # if not on I2C bus
+# useDisplay = True  # or 'I2C'
 # SPI pins
 #S_CLKI = 'P19'  # brown D0
 #S_MOSI = 'P18'  # white D1
@@ -53,18 +54,18 @@ useSSD = 'I2C'
 #S_CS   = 'P17'  # blew   CS
 
 # SDA wire is white, SCL wire is yellow or gray
-I2Cpins = [('P23','P22')] # I2C pins [(SDA,SCL), ...]
-I2Cdevices = [
-        ('BME280',0x76),('BME280',0x77), # BME serie Bosch
-        ('SHT31',0x44),('SHT31',0x45),   # Sensirion serie
-        ('SSD1306',0x3c)                 # oled display
-    ]
+# I2Cpins = [('P23','P22')] # I2C pins [(SDA,SCL), ...]
+# I2Cdevices = [
+#         ('BME280',0x76),('BME280',0x77), # BME serie Bosch
+#         ('SHT31',0x44),('SHT31',0x45),   # Sensirion serie
+#         ('SSD1306',0x3c)                 # oled display
+#    ]
 
-# uart = [] # uart allocations number from 0 (0,1,2) PCB nr TTL 3, 2, 1
-uart = [-1] # leave 0 free (USB TTL expansion board)  (Tx, Rx), ...
-UARTpins=[('P4','P3'),('P11','P10')] # default UART pins used by auto detect device
+# uart allocations number from 0 (0,1,2) PCB nr TTL 3, 2, 1
+# uart = [-1] # leave 0 free (USB TTL expansion board)  (Tx, Rx), ...
+# UARTpins=[('P4','P3'),('P11','P10')] # default UART pins used by auto detect device
 
-useGPS = 'UART'      # uart
+# useGPS = 'UART'      # None or False if not present
 # G_Tx = 'P11'    # white GPS Rx
 # G_Rx = 'P10'    # yellow GPS Tx
 
@@ -73,10 +74,10 @@ useGPS = 'UART'      # uart
 # calibration Taylor factors
 #calibrate = None # or e.g. { 'temperature': [-6.2,1], 'pm1': [-20.0,0.5], ...}
 
-useDust = 'UART'     # UART
-dust = 'PMS7003'     # define 0 if not
+# useDust = 'UART'     # UART, use False if not present
+#dust = 'PMS7003'     # define 0 if not
 # D_Tx = 'P4'     # yellow Rx module
 # D_Rx = 'P3'     # white Tx module
 #sampling = 60  # secs, default dust sampling timing
-# Dext = '' # only PMS: '_cnt' for pcs/0.1 dm3 (dflt: ug/m3)
-Dext = '_cnt'   # send PM count to database server
+# Dext = '' # only Plantower/Sensirion: '_cnt' for pcs/0.1 dm3 (dflt: ug/m3)
+#Dext = True   # send PM count PMnn_cnt to database server
