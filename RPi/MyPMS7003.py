cdcdd235c1992d0a7125976e6c42bc96a40d3d6b
Teus Hagen <teus@theunis.org>
2019-09-14 15:26:11 +0200

    update syntax error

--- RPi/MyPMS7003.py
+++ RPi/MyPMS7003.py
@@ -18,7 +18,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-# $Id: MyPMS7003.py,v 1.3 2019/09/14 13:16:26 teus Exp teus $
+# $Id: MyPMS7003.py,v 1.4 2019/09/14 13:24:58 teus Exp teus $
 
 # Defeat: output (moving) average PM count in period sample time seconds (dflt 60 secs)
 #         active (monitor) mode: continues read (200-600 msec) during sample time
@@ -39,7 +39,7 @@
     units: pcs/0.01qf, pcs/0.1dm3, ug/m3
 """
 modulename='$RCSfile: MyPMS7003.py,v $'[10:-4]
-__version__ = "0." + "$Revision: 1.3 $"[11:-2]
+__version__ = "0." + "$Revision: 1.4 $"[11:-2]
 
 # configurable options
 __options__ = [
@@ -589,7 +589,7 @@ def Add(conf):
     if len(data):
         if conf['debug']:
             print("raw,sensor=%s %s %d000" % (conf['type'][-7:],','.join(data),values['time']*1.0))
-        elif ('raw' in conf.keys()) and type(conf['raw'] is module):
+        elif ('raw' in conf.keys()) and (type(conf['raw']) is module):
             conf['raw'].publish(tag=conf['type'][-7:].lower(),data=','.join(data))
     return values
 
